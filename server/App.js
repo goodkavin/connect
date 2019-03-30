4aeb62b0b0c896aa53a1497dc22b45cfc9822b7b
Kavin Sermsaksakoon <goodkavin@gmail.com>
2019-03-30 15:07:19 +0700

    change port to 4000

--- server/App.js
+++ server/App.js
@@ -9,7 +9,7 @@ const SELECT_ALL_PWD_QUERY = 'SELECT * FROM source LIMIT 100';
 
 // const {getHomePage} = require('./routes/index');
 // const {addPlayerPage, addPlayer, deletePlayer, editPlayer, editPlayerPage} = require('./routes/player');
-const port = 3306;
+const port = 4000;
 
 // create connection to database
 // the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
